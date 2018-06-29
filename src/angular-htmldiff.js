217a8926af7cf87f3106ecf9db1e9ec5242cfefc
Moesjarraf J <moesjarraf@gmail.com>
2018-06-29 17:23:05 +0200

    Revert "Optionally normalize html before comparing" (#7)

--- src/angular-htmldiff.js
+++ src/angular-htmldiff.js
@@ -10,21 +10,6 @@
     var module = angular.module('htmldiff', []);
 
     module.service('htmldiff', [function () {
-        var minify = (function () {
-            return typeof require !== 'undefined' ? require('html-minifier').minify : null;
-        })();
-
-        function sanitize(html) {
-            var minified = minify ? minify(html, {
-                collapseWhitespace: true,
-                decodeEntities: true
-            }) : html;
-
-            return minified.replace(/\s+/g, ' ');
-        }
-
-        return function (before, after) {
-            return htmldiff(sanitize(before), sanitize(after));
-        }
+        return htmldiff;
     }]);
 }();
